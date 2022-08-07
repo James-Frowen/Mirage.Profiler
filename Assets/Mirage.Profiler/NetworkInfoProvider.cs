52a7bb4927e009eec5839e30c94bee1345e4a4b9
James Frowen <jamesfrowendev@gmail.com>
2022-08-07 16:44:24 +0100

    feat: adding rpc name to message info

--- Assets/Mirage.Profiler/NetworkInfoProvider.cs
+++ Assets/Mirage.Profiler/NetworkInfoProvider.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-
 namespace Mirage.NetworkProfiler
 {
     /// <summary>
@@ -7,8 +5,9 @@ namespace Mirage.NetworkProfiler
     /// </summary>
     public interface INetworkInfoProvider
     {
-        GameObject GetGameObject(uint? netId);
         uint? GetNetId(NetworkDiagnostics.MessageInfo info);
+        NetworkIdentity GetNetworkIdentity(uint? netId);
+        string GetRpcName(NetworkDiagnostics.MessageInfo info);
     }
 
     public class NetworkInfoProvider : INetworkInfoProvider
@@ -36,7 +35,7 @@ namespace Mirage.NetworkProfiler
             }
         }
 
-        public GameObject GetGameObject(uint? netId)
+        public NetworkIdentity GetNetworkIdentity(uint? netId)
         {
             if (!netId.HasValue)
                 return null;
@@ -45,8 +44,33 @@ namespace Mirage.NetworkProfiler
                 return null;
 
             return _world.TryGetIdentity(netId.Value, out var identity)
-                ? identity.gameObject
+                ? identity
                 : null;
+        }
+
+        public string GetRpcName(NetworkDiagnostics.MessageInfo info)
+        {
+            switch (info.message)
+            {
+                case ServerRpcMessage msg:
+                    return GetRpcName(msg.netId, msg.componentIndex, msg.functionIndex);
+                case ServerRpcWithReplyMessage msg:
+                    return GetRpcName(msg.netId, msg.componentIndex, msg.functionIndex);
+                case RpcMessage msg:
+                    return GetRpcName(msg.netId, msg.componentIndex, msg.functionIndex);
+                default: return string.Empty;
+            }
+        }
+
+        private string GetRpcName(uint netId, int componentIndex, int functionIndex)
+        {
+            var identity = GetNetworkIdentity(netId);
+            if (identity == null)
+                return string.Empty;
+
+            var behaviour = identity.NetworkBehaviours[componentIndex];
+            var rpc = behaviour.RemoteCallCollection.Get(functionIndex);
+            return rpc.name;
         }
     }
 }
